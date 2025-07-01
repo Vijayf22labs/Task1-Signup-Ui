4f09c4b7bd8c10b78ec0e0b33fedaf2a02f90a92
Vijay0254 <vijayabijeet254@gmail.com>
2025-07-01 18:12:08 +0530

    chore: Updated Font properties to built in

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,15 +5,15 @@ const Input = ({ name, id, type, label }) => {
   return (
     <div className='flex flex-col gap-y-1'>
         {type != "password" ?
-            <label className='font-[400] text-[16px] text-[#666666]' htmlFor={name}>{label}</label> :
+            <label className='font-normal text-base text-secondary' htmlFor={name}>{label}</label> :
             <div className='flex items-center justify-between'>
-                <label className='font-[400] text-[16px] text-[#666666]' htmlFor={name}>{label}</label>
-                <p className='text-[14px] sm:text-[18px] text-[#666666] font-[400] flex mr-[10px] items-center gap-x-1'><EyeOff className='size-4' /> Hide</p>
+                <label className='font-normal text-base text-tertiary' htmlFor={name}>{label}</label>
+                <p className='text-sm sm:text-lg text-tertiary font-normal flex mr-[10px] items-center gap-x-1'><EyeOff className='size-4' /> Hide</p>
             </div>
         }
         <input className='border outline-none border-gray-300 rounded-lg py-2 px-3' type={type} id={id} name={name} />
         {type == "password" &&
-            <p className='text-[14px] text-[#666666] mt-[-3px] font-[400]'>Use 8 or more characters with a mix of letters, numbers & symbols</p>}
+            <p className='text-sm text-tertiary mt-[-3px] font-normal'>Use 8 or more characters with a mix of letters, numbers & symbols</p>}
     </div>
   )
 }
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -7,33 +7,33 @@ const RightSide = () => {
     <div className='sm:w-1/2 flex justify-center'>
         <div className='flex w-[90%] sm:w-[75%] lg:w-[70%] flex-col gap-y-5'>
             <div className='text-center sm:text-start'>
-                <h1 className='text-[32px] font-[500] text-[#333333]'>Create an account</h1>
-                <p className='text-[16px] font-[400] text-[#333333]'>Already have an account? <a href="#" className='underline text-[#111111] font-[400]'>Log in</a></p>
+                <h1 className='text-3xl font-medium text-secondary'>Create an account</h1>
+                <p className='text-base font-normal text-secondary'>Already have an account? <a href="#" className='underline text-primary font-normal'>Log in</a></p>
             </div>
-
+``
             <div className='flex flex-col gap-y-4'>
                 <Input name={"username"} id={"username"} type={"text"} label={"User name"} />
                 <Input name={"email"} id={"email"} type={"email"} label={"Email"} />
                 <Input name={"password"} id={"password"} type={"password"} label={"Password"} />
             </div>
 
-            <p className='max-w-72 text-[16px] my-2 lg:my-3 font-[400] text-[#666666]'>By creating an account you agree to our <span className='text-[#111111] underline'>Terms of use</span> and <span className='text-[#111111] underline'>privacy Policy</span></p>
+            <p className='max-w-72 text-base my-2 lg:my-3 font-normal text-tertiary'>By creating an account you agree to our <span className='text-[#111111] underline'>Terms of use</span> and <span className='text-[#111111] underline'>privacy Policy</span></p>
 
-            <div className='px-3 py-2 max-w-88 rounded-2xl flex items-center justify-between border border-[#333333]'>
+            <div className='px-3 py-2 max-w-88 rounded-2xl flex items-center justify-between border border-secondary'>
                 <div className='flex items-center gap-2'>
-                    <input className='size-[18px] accent-[#34A853]' type="checkbox" id='notRobot' />
-                    <label className='text-[16px] text-[#333333] font-[300]' htmlFor="notRobot">I'm not a robot</label>
+                    <input className='size-lg accent-green-600' type="checkbox" id='notRobot' />
+                    <label className='text-base text-secondary font-light' htmlFor="notRobot">I'm not a robot</label>
                 </div>
 
                 <img className='w-[60px] h-[46px]' src={captcha} alt="captcha" />
             </div>
 
             <div className='flex items-center justify-center sm:justify-start'>
-                <button className='rounded-full text-white font-[500] text-[22px] px-7 py-3 bg-neutral-400'>
+                <button className='rounded-full text-white font-medium text-xl px-7 py-3 bg-neutral-400'>
                     Create an account
                 </button>
             </div>
-            <p className='text-[16px] text-center sm:text-start font-[400] text-[#333333] mt-[-15px]'>Already have an account? <a href="#" className='underline text-[#111111] font-medium'>Log in</a></p>
+            <p className='text-base text-center sm:text-start font-normal text-secondary mt-[-15px]'>Already have an account? <a href="#" className='underline text-primary font-medium'>Log in</a></p>
         </div>
     </div>
   )
